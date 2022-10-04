9d99c07e16c18695f8a067b522621ba397ad34a1
Vinícius Leivas <vinicius.leivas@dbseller.com.br>
2022-10-04 11:48:35 -0300

    04/10/2022   - Update

--- bashscript.bash
+++ bashscript.bash
@@ -1,1 +1,4 @@
-command git diff origin/master origin/$
+branch_ref= command git rev-parse --abrev-ref HEAD;
+git_command="git fetch --all -p -q | git diff main ${branc_ref} --name-only"
+
+eval $git_command;
