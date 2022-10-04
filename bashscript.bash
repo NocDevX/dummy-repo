f149ded6644f33760915c72be2f1d369950bab5c
Vinícius Leivas <vinicius.leivas@dbseller.com.br>
2022-10-04 12:42:51 -0300

    04/10/2022   - Update

--- bashscript.bash
+++ bashscript.bash
@@ -1,5 +1,5 @@
 eval "git fetch --all -p -q";
-git_command="git diff main $(git rev-parse HEAD) --name-only";
+git_command="git diff origin/main $(git rev-parse HEAD) --name-only";
 
 for diff_file in $($git_command); do
     eval "vendor/bin/phpcs --standard=PSR12 $diff_file";
