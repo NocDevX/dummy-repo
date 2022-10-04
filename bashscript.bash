a1f17b4b19a9b01fd217a23eaa05f5a91b716ea4
Vinícius Leivas <vinicius.leivas@dbseller.com.br>
2022-10-04 16:07:20 -0300

    04/10/2022   - Update

--- bashscript.bash
+++ bashscript.bash
@@ -1,6 +1,14 @@
-eval "git fetch --all -p -q";
-git_command="git diff origin/main $(git rev-parse HEAD) --name-only";
+# eval "git fetch --all -p -q";
+# git_command="git diff origin/main $(git rev-parse HEAD) --name-only";
 
-for diff_file in $($git_command); do
-    eval "vendor/bin/phpcs --standard=PSR12 $diff_file";
-done
+# for diff_file in $($git_command); do
+#     eval "vendor/bin/phpcs --standard=PSR12 $diff_file";
+#     echo "";
+# done
+
+COMMITS_BEHIND=$(git rev-list --count $(git rev-parse HEAD)..origin/main);
+
+if [[ $COMMITS_BEHIND -gt 0 ]]; then
+    echo "Branch desnivelada à master, $COMMITS_BEHIND commit(s) atrás."
+    exit 1;
+fi
