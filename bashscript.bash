2a172dff7fb5fecc973d56e58940d7c943509adb
Vinícius Leivas <vinicius.leivas@dbseller.com.br>
2022-10-04 16:28:16 -0300

    04/10/2022   - Update

--- bashscript.bash
+++ bashscript.bash
@@ -6,7 +6,8 @@
 #     echo "";
 # done
 
-COMMITS_BEHIND=$(git rev-list --count $(git rev-parse HEAD)..origin/main);
+# COMMITS_BEHIND=$(git rev-list --count $(git rev-parse HEAD)..origin/main);
+COMMITS_BEHIND=$(git rev-list --count origin/test-bash..origin/main);
 
 if [[ $COMMITS_BEHIND -gt 0 ]]; then
     echo "Branch desnivelada à master, $COMMITS_BEHIND commit(s) atrás."
